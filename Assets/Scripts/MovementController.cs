cc841d2bbf3ba5f51eec7749ddd5fcb912d13cad
Seth <seth@seths-mbp.mynetworksettings.com>
2024-01-14 10:07:41 -0500

    [player-movement] - add vertical movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -34,7 +34,7 @@ public class MovementController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 
     void Update()
