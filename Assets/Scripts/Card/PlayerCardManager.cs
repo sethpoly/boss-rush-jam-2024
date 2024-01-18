82bfa4e07723ad4fae2c2a234233700e9a0b368c
Seth <seth@seths-mbp.mynetworksettings.com>
2024-01-17 20:52:45 -0500

    [card-deck] - add deck and drawing animation

--- Assets/Scripts/Card/PlayerCardManager.cs
+++ Assets/Scripts/Card/PlayerCardManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,6 +13,7 @@ class PlayerCardManager: MonoBehaviour
     public Player player;
     public GameObject cardPrefab;
     public Transform playerHand;
+    public GameObject deck;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -41,23 +43,12 @@ class PlayerCardManager: MonoBehaviour
     private void ResetAndCreateDeck()
     {
         cardsInDeck.Clear();
-        float? lastXLocation = null;
         for(int i = 0; i < 5; i++)
         {
-            if(lastXLocation == null)
-            {
-                lastXLocation = playerHand.position.x - 4f;
-            } 
-            else 
-            {
-                lastXLocation -= -1.5f;
-            }
             var card = new MovementSpeedCard("Speed x" + i, 1, i);
-            var spawnLocation = new Vector3(lastXLocation.Value, playerHand.position.y, 0);
-            var cardPrefab = Instantiate(this.cardPrefab, spawnLocation, Quaternion.identity);
+            var cardPrefab = Instantiate(this.cardPrefab, deck.transform.position, Quaternion.identity);
             var controller = cardPrefab.GetComponent<CardController>();
             controller.card = card;
-            controller.SetSortOrder( i % 10);
             controller.MouseClickOccuredOnCardWithId += OnCardMouseClick;
             cardsInDeck.Add(cardPrefab);
         }
@@ -72,6 +63,16 @@ class PlayerCardManager: MonoBehaviour
         if (cardsInDeck.Count > 0) 
         {
             int index = cardsInDeck.Count - 1;
+
+            // Draw animation
+            float xOffset = 1.5f; // Adjust this value to control the spacing
+            float y = playerHand.position.y;
+            float x = playerHand.position.x - 4f + (cardsInHand.Count + 1) * xOffset;
+            
+            iTween.MoveTo(cardsInDeck[index], iTween.Hash("y", y, "x", x, "time", 1, "islocal", true));  
+
+            var controller = cardsInDeck[index].GetComponent<CardController>();
+            controller.SetSortOrder( cardsInHand.Count + 1 % 10);
             cardsInHand.Add(cardsInDeck[index]);
             cardsInDeck.RemoveAt(index);
             Debug.Log("Player drew card: " + GetController(cardsInHand[^1]).card.cardName);
