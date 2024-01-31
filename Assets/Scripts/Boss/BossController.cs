626f6fca15451e4c352c823afb610b319e57cc26
Seth <seth@seths-mbp.mynetworksettings.com>
2024-01-30 19:16:38 -0500

    [boss-health] - add boss takeDamage logic

---
+++ Assets/Scripts/Boss/BossController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossController : MonoBehaviour
+{
+    public float maxHitPoints;
+    public float currentHitPoints;
+
+    public Image healthBar;
+
+    void Awake()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(1f);
+        }
+    }
+
+    public void TakeDamage(float hitPoints) 
+    {
+        currentHitPoints -= hitPoints;
+        healthBar.fillAmount = currentHitPoints / maxHitPoints;
+    }
+}
