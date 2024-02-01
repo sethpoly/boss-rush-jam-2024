986904e15e5ffa1a3ce78df712e4d4c0036c5316
Seth <seth@seths-mbp.mynetworksettings.com>
2024-01-31 20:38:38 -0500

    [boss-design] - add coll logic

--- Assets/Scripts/BulletHellGenerator.cs
+++ Assets/Scripts/BulletHellGenerator.cs
@@ -15,6 +15,7 @@ public class BulletHellGenerator : MonoBehaviour
     public Material material;
     public Sprite sprite;
     public float spinSpeed;
+    public LayerMask collisionLayerMask;
 
     private float angle;
     private float time;
@@ -52,6 +53,14 @@ public class BulletHellGenerator : MonoBehaviour
             mainModule.startSpeed = speed;
             mainModule.maxParticles = 100000;
             mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
+
+            var collision = system.collision;
+            collision.type = ParticleSystemCollisionType.World;
+            collision.mode = ParticleSystemCollisionMode.Collision2D;
+            collision.lifetimeLoss = 1; //(if this is at 1, the paricle will "die" after colliding)
+            collision.collidesWith = collisionLayerMask; //(the layer you want collision happening on)
+            collision.sendCollisionMessages = true;
+            collision.enabled = true;
 
             var emission = system.emission;
             emission.enabled = false;
