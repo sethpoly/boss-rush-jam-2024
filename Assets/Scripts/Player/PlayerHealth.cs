a2020db1204c763a259892ee532b6ac2d380659a
Seth <seth@seths-mbp.mynetworksettings.com>
2024-02-02 12:46:30 -0500

    [player-iframe] - add player iframe

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,21 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth: MonoBehaviour {
     [SerializeField] GameManager gameManager;
+    [SerializeField] SpriteRenderer sr;
     public float maxHitPoints;
     public float currentHitPoints;
     public Image healthBar;
     public float defenseBuffMultiplier = 0;
     public BossController bossController;
+    public bool invincibleActive = false;
+    private Color originalSpriteColor;
 
     void Awake()
     {
         currentHitPoints = maxHitPoints;
+        originalSpriteColor = sr.color;
     }
 
     public void TakeDamage(float hitPoints) 
     {
+        if(invincibleActive) return;
         float ignoredDamage = hitPoints * defenseBuffMultiplier;
         float damageToTake = hitPoints - ignoredDamage;
         currentHitPoints -= damageToTake;
@@ -31,7 +37,7 @@ public class PlayerHealth: MonoBehaviour {
              Destroy(transform.parent.gameObject);
             // TODO: Go to menu
         } else {
-
+            StartCoroutine(Flash());
         }
     }
 
@@ -60,5 +66,16 @@ public class PlayerHealth: MonoBehaviour {
     void OnParticleCollision(GameObject other)
     {
         TakeDamage(bossController.damageOutput);
+    }
+
+    private IEnumerator Flash() 
+    {
+        invincibleActive = true;
+        var tmpColor = originalSpriteColor;
+        tmpColor.a = 0.5f;
+        sr.color = tmpColor;
+        yield return new WaitForSeconds(1.5f);
+        sr.color = originalSpriteColor;
+        invincibleActive = false;
     }
 }
